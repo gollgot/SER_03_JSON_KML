f900efa835b873f850a263c12da3f8347e2d60be
G-Houlmann <gildas.houlmann@gmail.com>
2020-05-27 15:02:44 +0200

    comment Utils.java

--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -2,6 +2,12 @@ import org.json.simple.JSONArray;
 
 public class Utils {
 
+    /**
+     * transforme un JSONArray de coordonnées en une chaîne de caractères contenant les mêmes coordonnées,
+     * formatées pour du KML
+     * @param outerBoundaries JSONArray à convertir en String
+     * @return la chaîne de caractères générée
+     */
     public static String convertOuterBoundariesToStringCoordinates(JSONArray outerBoundaries){
         StringBuilder result = new StringBuilder();
         for(Object coordinatesObject : outerBoundaries){
